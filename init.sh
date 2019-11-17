03e77ce29e4b194c72c3c5d5b9e7a76a3087cdd8
SilentGo <51835075+SilentGo@users.noreply.github.com>
2019-11-17 20:18:52 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed -i "s|input_username|$LANPROXY_USERNAME|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
-sed -i "s|input_password|$LANPROXY_PASSWORD|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
+sed -i "s|admin|$LANPROXY_USERNAME|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
+sed -i "s|admin|$LANPROXY_PASSWORD|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
 
 # starting 
 sh /lanproxy-server/start.sh
