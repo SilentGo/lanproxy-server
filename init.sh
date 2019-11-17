3fd6a0aa4ef094cbe31339a09567eb0689e4d434
SilentGo <51835075+SilentGo@users.noreply.github.com>
2019-11-17 22:05:32 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $LANPROXY_USERNAME ];then
-    sed -i "14s|admin|$LANPROXY_USERNAME|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
+    sed -i "14s|=admin|=$LANPROXY_USERNAME|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
 fi
 if [ $LANPROXY_PASSWORD ];then    
-    sed -i "15s|admin|$LANPROXY_PASSWORD|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
+    sed -i "15s|=admin|=$LANPROXY_PASSWORD|g" /lanproxy-server/distribution/proxy-server-0.1/conf/config.properties
 fi
 # starting 
 sh /lanproxy-server/start.sh
